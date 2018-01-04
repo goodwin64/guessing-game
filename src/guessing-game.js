bb8a69e86ccb902163f795c8f6366eca92d72372
Max Donchenko <wereya2@yandex.ru>
2018-01-04 20:59:37 +0200

    basic implementation via "average" method

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,20 +2,28 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        const { min, max } = this;
+        return average(min, max);
     }
 
     lower() {
-
+        const { min, max } = this;
+        this.max = average(min, max);
     }
 
     greater() {
-
+        const { min, max } = this;
+        this.min = average(min, max);
     }
+}
+
+function average(min, max) {
+    return Math.round((min + max) / 2);
 }
 
 module.exports = GuessingGame;
